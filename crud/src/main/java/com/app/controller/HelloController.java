4fe241be11b5120ec8269f2dfe41e934f3e5a303
MD2105 <manishdoble5@gmail.com>
2021-04-25 14:45:42 +0530

    adding spring file (backend)

---
+++ crud/src/main/java/com/app/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+      public HelloController() {
+    	  System.out.println("In a Hello controlelr");
+      }
+}

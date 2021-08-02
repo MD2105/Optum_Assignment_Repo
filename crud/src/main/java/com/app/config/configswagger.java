27da2af6a2d92ea65a4dd34d05acdd7c9e4c6410
Manish Doble <62184156+MD2105@users.noreply.github.com>
2021-08-02 16:31:04 +0530

    Update configswagger.java

--- crud/src/main/java/com/app/config/configswagger.java
+++ crud/src/main/java/com/app/config/configswagger.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class configswagger {                                    
     @Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
+        return new Docket(DocumentationType.SWAGGER_2)  ...
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
           .paths(PathSelectors.any())                          
